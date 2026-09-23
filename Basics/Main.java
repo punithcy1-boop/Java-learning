dec89b3aa25065089c2dad9f146ca1cfb3363677
punithcy1-boop <punithcy1@gmail.com>
2026-09-23 23:04:49 +0530

    Create Basics/Main.java
    Added first Java program

---
+++ Basics/Main.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+
+        // This is a single-line comment
+        System.out.println("Hello Java");
+
+        /*
+         This is a
+         multi-line comment
+        */
+
+        System.out.println("I am learning Java");
+    }
+}

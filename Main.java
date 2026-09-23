0c78fa9dec467218a0e5905ecd3b9859461c64b1
punithcy1-boop <punithcy1@gmail.com>
2026-09-23 22:50:02 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        int a = 19;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+    }
+}
